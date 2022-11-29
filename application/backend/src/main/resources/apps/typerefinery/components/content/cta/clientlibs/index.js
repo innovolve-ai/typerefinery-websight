eb6b5e1f724796d2a1b7897cd0270e8a561490cc
Rohit gupta <rogupta@aapnainfotech.com>
2022-11-29 11:57:14 +0530

    add js file

---
+++ application/backend/src/main/resources/apps/typerefinery/components/content/cta/clientlibs/index.js
@@ -0,0 +1,9 @@
+// cta javascript
+(function (vueapp) {
+  // vueapp.methods.clickevent = clickevent1;
+  vueapp.firstname = "rohit";
+  console.log("cta javascript");
+  console.log("vueapp", vueapp);
+  console.log("app", vueapp.App);
+  console.log("vue", vueapp.Vue);
+})(vueapp);
